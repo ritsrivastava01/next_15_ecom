13193bc5c392f09877b77f25bea7b8f0a12f28fe
Ritesh <ritsrivastava@gmail.com>
2025-01-02 10:03:03 +0100

    feat: add organize imports and sort imports plugins to Prettier

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,14 +12,15 @@ const Button = forwardRef<HTMLButtonElement, JSX.IntrinsicElements['button']>(
       <button
         ref={ref}
         className={classMerge(
-          'bg-gray-800 hover:bg-gray-600 text-white py-2 px-4 rounded-lg',
-          className
+          'rounded-lg bg-gray-800 px-4 py-2 text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-opacity-50',
+          className,
         )}
-        {...props}>
+        {...props}
+      >
         {title}
       </button>
     );
-  }
+  },
 );
 
 Button.displayName = 'Button';
